06ab1de125ccec5b977cbd142b0847a685989f1c
hj7321 <oboki2001@naver.com>
2024-06-28 03:45:21 +0900

    feat: 화면에 각 나라들에 대한 기본 정보 나타나게 하기

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,5 +1,26 @@
-const CountryCard = () => {
-  return <div>CountryCard 컴포넌트</div>
+import { StCap, StCon, StImg, StLi, StUl } from "../style/CountryCardStyle";
+import { Country } from "../types/country.type";
+
+interface CountryCardProps {
+  countries: Country[];
 }
+
+const CountryCard: React.FC<CountryCardProps> = ({ countries }) => {
+  const onHandleToggle = () => {
+    //
+  };
+
+  return (
+    <StUl>
+      {countries.map((country) => (
+        <StLi key={country.name} onClick={onHandleToggle}>
+          <StImg src={country.flags} height="100px" />
+          <StCon>{country.name}</StCon>
+          <StCap>수도: {country.capital}</StCap>
+        </StLi>
+      ))}
+    </StUl>
+  );
+};
 
 export default CountryCard;
