b1fb15451f7c218d71af80af4c58d07177a361ff
hj7321 <oboki2001@naver.com>
2024-06-28 03:39:33 +0900

    add + style: CountryCard 컴포넌트에 대한 스타일 파일 생성 및 스타일 설정

---
+++ src/style/CountryCardStyle.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const StUl = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 30px;
+`;
+
+export const StLi = styled.li`
+  box-shadow: 3px 3px 5px black;
+  background-color: aliceblue;
+  border-radius: 10px;
+  padding: 20px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StImg = styled.img`
+  margin-bottom: 10px;
+`;
+
+export const StCon = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const StCap = styled.p`
+  font-size: 16px;
+`;
