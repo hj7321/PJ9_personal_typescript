64c7d8442d11b1e01f67a2dcd4e5d8edfa5588de
hj7321 <oboki2001@naver.com>
2024-06-28 03:44:49 +0900

    feat: 데이터를 페칭하는 함수를 호출해서 데이터를 불러온 후, CountryCard 컴포넌트에 데이터 넘겨주기

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,5 +1,40 @@
-const CountryList = () => {
-  return <div>CountryList 컴포넌트</div>
-}
+import { useEffect, useState } from "react";
+import { getCountries } from "../api/countries";
+import { Country } from "../types/country.type";
+import CountryCard from "./CountryCard";
+import { StDiv } from "../style/CountryListStyle";
+
+const CountryList: React.FC = () => {
+  const [countries, setCountries] = useState<Country[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getCountries();
+        setCountries(data);
+        console.log(data);
+      } catch (error) {
+        console.error("에러: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!countries) return <div>Loading...</div>;
+
+  return (
+    <>
+      <section>
+        <h1>💖Favorite Countries💖</h1>
+        <StDiv></StDiv>
+      </section>
+      <section>
+        <h1>🌍Countries🌍</h1>
+        <StDiv>{countries && <CountryCard countries={countries} />}</StDiv>
+      </section>
+    </>
+  );
+};
 
 export default CountryList;
