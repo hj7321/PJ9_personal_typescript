046ee5470edb3f8dd6538cb3e3d59a1e86ff3775
hj7321 <oboki2001@naver.com>
2024-06-28 03:43:16 +0900

    chore: 모든 속성에 타입 지정해보고, 사용할 데이터만을 포함한 Country 인터페이스 만들기

--- src/types/country.type.ts
+++ src/types/country.type.ts
@@ -1,6 +1,103 @@
+// type NativeName = {
+//   [key: string]: {
+//     official: string;
+//     common: string;
+//   };
+// };
+
+// type Currencies = {
+//   [key: string]: {
+//     name: string;
+//     symbol: string;
+//   };
+// };
+
+// type Idd = {
+//   root: string;
+//   suffixes: string[];
+// };
+
+// type Translations = {
+//   [key: string]: {
+//     official: string;
+//     common: string;
+//   };
+// };
+
+// type Demonyms = {
+//   [key: string]: {
+//     f: string;
+//     m: string;
+//   };
+// };
+
+// type Maps = {
+//   googleMaps: string;
+//   openStreetMaps: string;
+// };
+
+// type Car = {
+//   signs: string[];
+//   side: string;
+// };
+
+// type Flags = {
+//   png: string;
+//   svg: string;
+// };
+
+// type CapitalInfo = {
+//   latlng: number[];
+// };
+
+// type PostalCode = {
+//   format: string;
+//   regex: string;
+// };
+
+// export interface Country {
+//   name: {
+//     common: string;
+//     official: string;
+//     nativeName: NativeName;
+//   };
+//   tld: string[];
+//   cca2: string;
+//   ccn3: string;
+//   cca3: string;
+//   independent: boolean;
+//   status: string;
+//   unMember: boolean;
+//   currencies: Currencies;
+//   idd: Idd;
+//   capital: string[];
+//   allSpellings: string[];
+//   region: string;
+//   subregion: string;
+//   languages: {
+//     [key: string]: string;
+//   };
+//   translations: Translations;
+//   latlng: number[];
+//   landlocked: boolean;
+//   area: number;
+//   demonyms: Demonyms;
+//   flag: string;
+//   maps: Maps;
+//   population: number;
+//   car: Car;
+//   timezones: string[];
+//   continents: string[];
+//   flags: Flags;
+//   coatOfArms: object;
+//   startOfWeek: string;
+//   capitalInfo: CapitalInfo;
+//   postalCode: PostalCode;
+// }
+
 export interface Country {
+  flags: string;
   name: string;
   capital: string;
-  png: string;
+  isFavorite: boolean;
 }
-// 이거 모든 타입 다 지정해보기
