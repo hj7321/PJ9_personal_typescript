0bc0b4718777a30273557572072f1abfb8719e50
hj7321 <oboki2001@naver.com>
2024-06-28 03:43:55 +0900

    feat: API를 호출하는 로직 작성

--- src/api/countries.tsx
+++ src/api/countries.tsx
@@ -2,7 +2,19 @@ import axios from "axios";
 import { Country } from "../types/country.type";
 
 export const getCountries = async (): Promise<Country[]> => {
-  const { data } = await axios.get("https://restcountries.com/v3.1/all");
-  console.log(data);
-  return data;
+  try {
+    const { data } = await axios.get("https://restcountries.com/v3.1/all");
+    const countriesData: Country[] = data.map((country: any) => ({
+      flags: country.flags.png,
+      name: country.name.common,
+      capital: Array.isArray(country.capital)
+        ? country.capital[0]
+        : country.capital,
+      isFavorite: false,
+    }));
+    return countriesData;
+  } catch (error) {
+    console.error("에러: ", error);
+    throw error;
+  }
 };
