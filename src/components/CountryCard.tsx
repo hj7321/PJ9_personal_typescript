e1da40a52e9ace064f7d445f1171d9ce65089805
hj7321 <oboki2001@naver.com>
2024-06-28 09:45:06 +0900

    feat: 토글 기능 추가

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -3,17 +3,21 @@ import { Country } from "../types/country.type";
 
 interface CountryCardProps {
   countries: Country[];
+  onToggleFavorite: (countryName: string) => void;
 }
 
-const CountryCard: React.FC<CountryCardProps> = ({ countries }) => {
-  const onHandleToggle = () => {
-    //
+const CountryCard: React.FC<CountryCardProps> = ({
+  countries,
+  onToggleFavorite,
+}) => {
+  const onHandleToggle = (countryName: string) => {
+    onToggleFavorite(countryName);
   };
 
   return (
     <StUl>
       {countries.map((country) => (
-        <StLi key={country.name} onClick={onHandleToggle}>
+        <StLi key={country.name} onClick={() => onHandleToggle(country.name)}>
           <StImg src={country.flags} height="100px" />
           <StCon>{country.name}</StCon>
           <StCap>수도: {country.capital}</StCap>
