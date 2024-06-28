64cb4aa14d2e0439418cbf44d425524412d2f0db
hj7321 <oboki2001@naver.com>
2024-06-28 10:23:20 +0900

    chore: API 호출하는 곳에서 데이터 조작하는 코드를 없앰

--- src/api/countries.tsx
+++ src/api/countries.tsx
@@ -4,15 +4,15 @@ import { Country } from "../types/country.type";
 export const getCountries = async (): Promise<Country[]> => {
   try {
     const { data } = await axios.get("https://restcountries.com/v3.1/all");
-    const countriesData: Country[] = data.map((country: Country) => ({
-      flags: country.flags.png,
-      name: country.name.common,
-      capital: Array.isArray(country.capital)
-        ? country.capital[0]
-        : country.capital,
-      isFavorite: false,
-    }));
-    return countriesData;
+    // const countriesData: Country[] = data.map((country: Country) => ({
+    //   flags: country.flags.png,
+    //   name: country.name.common,
+    //   capital: Array.isArray(country.capital)
+    //     ? country.capital[0]
+    //     : country.capital,
+    // }));
+    // return countriesData;
+    return data;
   } catch (error) {
     console.error("에러: ", error);
     throw error;
