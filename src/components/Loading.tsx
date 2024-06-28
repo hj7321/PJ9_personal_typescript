c139fde1b46a1ab8c97b80ec720ad193a49e008f
hj7321 <oboki2001@naver.com>
2024-06-28 10:46:48 +0900

    style: CSS 로더 생성

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import "../style/Loading.css";
+
+const Loading: React.FC = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
