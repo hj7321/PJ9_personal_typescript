e1a8d884f3d9a89cb9f8b144b01d72701deb1694
hj7321 <oboki2001@naver.com>
2024-06-28 09:44:48 +0900

    feat: isFavorite 속성을 이용하여 토글 함수 생성

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getCountries } from "../api/countries";
 import { Country } from "../types/country.type";
 import CountryCard from "./CountryCard";
-import { StDiv } from "../style/CountryListStyle";
+import { StDiv, StSection } from "../style/CountryListStyle";
 
 const CountryList: React.FC = () => {
   const [countries, setCountries] = useState<Country[] | null>(null);
@@ -21,18 +21,51 @@ const CountryList: React.FC = () => {
     fetchData();
   }, []);
 
+  const onToggleFavorite = (countryName: string) => {
+    if (countries) {
+      setCountries(
+        countries.map((country) =>
+          country.name === countryName
+            ? { ...country, isFavorite: !country.isFavorite }
+            : country
+        )
+      );
+    }
+  };
+
   if (!countries) return <div>Loading...</div>;
+
+  const favoriteCountries = countries.filter((country) => country.isFavorite);
+  const nonFavoriteCountries = countries.filter(
+    (country) => !country.isFavorite
+  );
 
   return (
     <>
-      <section>
+      <StSection $top>
         <h1>💖Favorite Countries💖</h1>
-        <StDiv></StDiv>
-      </section>
-      <section>
+        <StDiv>
+          {favoriteCountries.length ? (
+            <CountryCard
+              countries={favoriteCountries}
+              onToggleFavorite={onToggleFavorite}
+            />
+          ) : (
+            ""
+          )}
+        </StDiv>
+      </StSection>
+      <StSection>
         <h1>🌍Countries🌍</h1>
-        <StDiv>{countries && <CountryCard countries={countries} />}</StDiv>
-      </section>
+        <StDiv>
+          {
+            <CountryCard
+              countries={nonFavoriteCountries}
+              onToggleFavorite={onToggleFavorite}
+            />
+          }
+        </StDiv>
+      </StSection>
     </>
   );
 };
