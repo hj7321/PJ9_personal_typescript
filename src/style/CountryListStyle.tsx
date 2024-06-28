945bedc72354f994b9d34c2c9226adb9d7a88e69
hj7321 <oboki2001@naver.com>
2024-06-28 09:43:35 +0900

    style: 스타일 추가

--- src/style/CountryListStyle.tsx
+++ src/style/CountryListStyle.tsx
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
+interface StSectionProps {
+  $top?: boolean;
+}
+
+export const StSection = styled.section<StSectionProps>`
+  margin-top: ${(props) => props.$top && "20px"};
+  margin-bottom: 40px;
+`;
+
 export const StDiv = styled.div`
   margin-top: 20px;
 `;
