53c69930cc732d1c1597009cbf750810cfd22960
hj7321 <oboki2001@naver.com>
2024-06-28 12:14:44 +0900

    feat: 정렬 기능 추가

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react";
 import { getCountries } from "../api/countries";
 import { CountryWithFavorite } from "../types/country.type";
 import CountryCard from "./CountryCard";
-import { StDiv, StSection } from "../style/CountryListStyle";
+import { StDiv, StSection, StSelect } from "../style/CountryListStyle";
 import Loading from "./Loading";
 
 const CountryList: React.FC = () => {
   const [countries, setCountries] = useState<CountryWithFavorite[] | null>(
     null
   );
+  const [sortOption, setSortOption] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await getCountries();
-        console.log(data);
         const newData = data.map((e) => ({ ...e, isFavorite: false }));
         setCountries(newData);
       } catch (error) {
@@ -39,6 +39,26 @@ const CountryList: React.FC = () => {
 
   if (!countries) return <Loading />;
 
+  const sortCountryCard = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(sortOption);
+    const value = event.target.value;
+    setSortOption(value);
+
+    if (value === "country") {
+      countries.sort((a, b) => {
+        const result = a.name.common.localeCompare(b.name.common);
+        return result;
+      });
+    } else if (value === "capital") {
+      countries.sort((a, b) => {
+        if (!a.capital || a.capital.length === 0) return 1;
+        if (!b.capital || b.capital.length === 0) return -1;
+        return a.capital[0].localeCompare(b.capital[0]);
+      });
+    }
+    setCountries(countries);
+  };
+
   const favoriteCountries = countries.filter((country) => country.isFavorite);
   const nonFavoriteCountries = countries.filter(
     (country) => !country.isFavorite
@@ -48,6 +68,13 @@ const CountryList: React.FC = () => {
     <>
       <StSection $top>
         <h1>💖Favorite Countries💖</h1>
+        <StSelect value={sortOption} onChange={sortCountryCard}>
+          <option value="" disabled style={{ display: "none" }}>
+            정렬 선택
+          </option>
+          <option value="country">나라 이름순</option>
+          <option value="capital">수도 이름순</option>
+        </StSelect>
         <StDiv>
           {favoriteCountries.length ? (
             <CountryCard
